88c09d847d8720a500323745df72a9c6a4997c3d
Antasnasiia <nastya.af@gmail.com>
2023-01-20 23:27:20 +0200

    install browser-sync and watch in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 const gulp = require('gulp');
+const browserSync= require('browser-sync').create();
+const watch = require ('gulp-watch');
 
 
+// Таск для отображения контента в реальном времени
+gulp.task('server', function() {
+    browserSync.init( {
+        server: {
+            baseDir: "./dev"
+        }
+    })
+});
+
+//Таск для отслеживаниями изменений в файлах
+gulp.task('watch', function() {
+    watch(['./dev/*.html', './dev/css/**/*.css']), gulp.parallel(browserSync.reload);
+});
+//Дефолтный запуск gulp
+gulp.task('default', gulp.parallel('server', 'watch'));
 
 
